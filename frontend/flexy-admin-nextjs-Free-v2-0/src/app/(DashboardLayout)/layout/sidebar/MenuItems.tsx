00cb638bb81f3615c3beeca2ec6036d6590bef67
haveyoumetdenz <55565206+haveyoumetdenz@users.noreply.github.com>
2024-04-03 22:03:55 +0800

    v1.00

---
+++ frontend/flexy-admin-nextjs-Free-v2-0/src/app/(DashboardLayout)/layout/sidebar/MenuItems.tsx
@@ -0,0 +1,28 @@
+import {
+  IconBoxMultiple, IconCircleDot, IconHome, IconInfoCircle, IconLayout, IconLayoutGrid, IconPhoto, IconPoint, IconStar, IconTable, IconUser
+} from "@tabler/icons-react";
+
+import { uniqueId } from "lodash";
+
+const Menuitems = [
+  {
+    id: uniqueId(),
+    title: "Dashboard",
+    icon: IconHome,
+    href: "/",
+  },
+  {
+    id: uniqueId(),
+    title: "Buttons",
+    icon: IconCircleDot,
+    href: "/ui-components/buttons",
+  },
+  {
+    id: uniqueId(),
+    title: "Forms",
+    icon: IconTable,
+    href: "/ui-components/forms",
+  }
+];
+
+export default Menuitems;
