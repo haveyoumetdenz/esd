bfb89c791e84643851563f28f6ec6b363c4a74b5
haveyoumetdenz <55565206+haveyoumetdenz@users.noreply.github.com>
2024-04-04 05:21:56 +0800

    5.21am

--- frontend/flexy-admin-nextjs-Free-v2-0/src/app/(DashboardLayout)/page.tsx
+++ frontend/flexy-admin-nextjs-Free-v2-0/src/app/(DashboardLayout)/page.tsx
@@ -4,48 +4,42 @@ import PageContainer from '@/app/(DashboardLayout)/components/container/PageCont
 // components
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
-import interactionPlugin, { Draggable, DropArg} from '@fullcalendar/interaction';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import googleCalendarPlugin from '@fullcalendar/google-calendar';
-import { use, useState } from 'react';
-
-let INITIAL_EVENTS = [
-  {
-      googleCalendarId:'https://calendar.google.com/calendar/ical/denzel.toh.2022%40smu.edu.sg/public/basic.ics',
-      className:'event'
-  }
-]
+import { useState } from 'react';
 
 export default function Home() {
+  const [events, setEvents] = useState([
+    {
+      googleCalendarId: '434c3a6f1d477740be91e0f73d98da3dfbcbef70d299c5a53c542d3e40e29de5@group.calendar.google.com',
+      className: 'event'
+    }
+  ]);
+  console.log(events)
   return (
-  <>
-  <iframe src="https://calendar.google.com/calendar/embed?src=denzel.toh.2022%40smu.edu.sg&ctz=Asia%2FSingapore" allowFullScreen width="100%" height="500px"></iframe>
-  {/* <nav className="flex justify-between mb-12 border-b border-violet-100 p-4 ">
-    <h1 className="font-bold text-2x1 text-gray-700">Calendar</h1>  
-  </nav>
-  <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <div className="grid grid-cols-10">
-      <div>
-      <FullCalendar
-        plugins={[
-          dayGridPlugin,
-          interactionPlugin,
-          timeGridPlugin,
-          googleCalendarPlugin
-        ]}
-        headerToolbar={{
-          left: 'prev,next today',
-          center: 'title',
-          right: 'dayGridMonth, timeGridWeek'
-        }}
-        initialView="dayGridWeek"
-        nowIndicator={true}
-        selectMirror={true}
-        googleCalendarApiKey='AIzaSyDtfQJvVR3xySXrLTVXgSnOCVFOprFcKyM'
-        eventSources={{googleCalendarId: "https://calendar.google.com/calendar/embed?src=denzel.toh.2022%40smu.edu.sg&ctz=Asia%2FSingapore"}}
-      />
-      </div>
-    </div>
-    </main> */}
-  </>
-)}
+    <>
+      <nav className="flex justify-between mb-12 border-b border-violet-100 p-4 ">
+        <h1 className="font-bold text-2xl text-gray-700">Calendar</h1>  
+      </nav>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="grid grid-cols-10">
+          <div>
+            <FullCalendar
+              plugins={[dayGridPlugin, timeGridPlugin, googleCalendarPlugin]}
+              headerToolbar={{
+                left: 'prev,next today',
+                center: 'title',
+                right: 'dayGridMonth,timeGridWeek'
+              }}
+              initialView="dayGridMonth"
+              nowIndicator={true}
+              selectMirror={true}
+              googleCalendarApiKey='AIzaSyCRTHDapEDol-cgIeerOAcaq_QptuM9F18'
+              eventSources={events}
+            />
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
